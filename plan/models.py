053f1bff84c93fcfa439d39f8a12c8e57c82fc33
lmarin4444 <lorena.soporte@gmail.com>
2019-06-26 17:44:46 -0400

    update actividades01

--- plan/models.py
+++ plan/models.py
@@ -579,11 +579,11 @@ class Actividades(models.Model):
     cantidad_convocada 			= models.IntegerField(default=0)
     
     	
-    verificadores               = models.ManyToManyField(Verificadores,default=1)
+    verificadores               = models.ManyToManyField(Verificadores)
     
     observaciones 		 		= models.TextField(default=0)
     planes_externos 	 		= models.ForeignKey(Planes_externos,blank=True, null=True)
-    planes_mineduc              = models.ManyToManyField(Planes_mineduc,default=1)
+    planes_mineduc              = models.ManyToManyField(Planes_mineduc)
     
     evaluacion  				= models.IntegerField(choices=EVALUACION,default=0)
         
